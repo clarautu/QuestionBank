0f79253206b1e5cbd4e49f10bbcc1f46a209d644
Zeremer <chashunt@isu.edu>
2022-09-15 13:27:13 -0600

    Class Creation
    Created the Database Class
    Created the Tag Class

---
+++ app/src/main/java/QuestionBank/Database.java
@@ -0,0 +1,29 @@
+package QuestionBank;
+
+public class Database {
+
+    public LinkedList<Question> GetQuestions() {
+
+    }
+
+    public LinkedList<Tag> GetTagsList() {
+
+    }
+
+    public Boolean CreateQuestion() {
+
+    }
+
+    public boolean RemoveQuestion(id:int) {
+
+    }
+
+    public String GetQuestion(id:int) {
+
+    }
+
+    public LinkedList<Question> GetTaggedQuestions(tagName:String) {
+
+    }
+
+}
