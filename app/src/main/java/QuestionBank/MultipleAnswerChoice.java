acd11085b4a239304733979a827ed7ad2af8a3b2
Eric Hill <hilleri2@isu.edu>
2022-10-06 13:16:10 -0600

    Removing unnessecary overloading

--- app/src/main/java/QuestionBank/MultipleAnswerChoice.java
+++ app/src/main/java/QuestionBank/MultipleAnswerChoice.java
@@ -5,13 +5,6 @@ import java.util.LinkedList;
 public class MultipleAnswerChoice extends Questions {
     //Fields
 
-    //Inherited fields
-    private int IdNumber;
-    private String Question;
-    private LinkedList<String> CorrectAnswers;
-    private LinkedList<String> PossibleAnswers;
-    private LinkedList<String> Tags;
-
     //Constructor
     protected MultipleAnswerChoice(int idNumber, String question, LinkedList<String> correctAnswers, LinkedList<String> possibleAnswers) {
         super(idNumber, question, correctAnswers, possibleAnswers);
--- app/src/main/java/QuestionBank/SingleMultipleChoice.java
+++ app/src/main/java/QuestionBank/SingleMultipleChoice.java
@@ -5,13 +5,6 @@ import java.util.LinkedList;
 public class SingleMultipleChoice extends Questions{
     //Fields
 
-    //Inherited Fields
-    private int IdNumber;
-    private String Question;
-    private LinkedList<String> CorrectAnswers;
-    private LinkedList<String> PossibleAnswers;
-    private LinkedList<String> Tags;
-
     //Constructor
     protected SingleMultipleChoice(int idNumber, String question, LinkedList<String> correctAnswers, LinkedList<String> possibleAnswers) {
         super(idNumber, question, correctAnswers, possibleAnswers);
