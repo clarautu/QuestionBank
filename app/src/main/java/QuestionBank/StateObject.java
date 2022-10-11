f077e8dad3c88d54c1a2f9035d8d07caf6aa05b0
Hunter Chase <chashunt@isu.edu>
2022-10-11 01:48:52 -0600

    Added StateManager
    Added a basic state manager

---
+++ app/src/main/java/QuestionBank/StateObject.java
@@ -0,0 +1,12 @@
+package QuestionBank;
+
+
+/**
+ * Used a vessel for game state info to be saved as a file
+ */
+public class StateObject {
+
+
+    //Variables // This all needs to be filled out for saving to retain a state
+    Object object;
+}
