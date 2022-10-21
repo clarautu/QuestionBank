7fa07ac1172bf757300bea23c96e9d5434f19df8
Hunter Chase <chashunt@isu.edu>
2022-10-21 01:57:43 -0600

    Changed state test

--- app/src/test/groovy/QuestionBank/StateManagerTest.groovy
+++ app/src/test/groovy/QuestionBank/StateManagerTest.groovy
@@ -1,4 +1,36 @@
 package QuestionBank
 
-class StateManagerTest {
+import org.codehaus.groovy.ast.VariableScope
+import spock.lang.Specification
+
+class StateManagerTest extends Specification {
+
+    // Variables
+    def stateManager;
+
+    // Setup
+    def setup() {
+        stateManager = Mock(StateManager)
+    }
+
+    // Tests
+
+    def "saveDataTest"() {
+        setup:
+        int a = 1;
+
+        when:
+        a += 1;
+
+        then:
+        a == 2;
+    }
+
+    def "createFileTest"() {
+
+    }
+
+    def "loadDataTest"() {
+
+    }
 }
