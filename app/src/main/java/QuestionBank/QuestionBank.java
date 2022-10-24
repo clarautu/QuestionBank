6191e694706b03bde893e0a832259028f8e5ae4a
Autumn Clark <clarautu@isu.edu>
2022-10-23 18:42:36 -0600

    Added a reset feature to question bank, to restore the bank to its default state

--- app/src/main/java/QuestionBank/QuestionBank.java
+++ app/src/main/java/QuestionBank/QuestionBank.java
@@ -13,7 +13,7 @@ public class QuestionBank {
         this.Questions = new LinkedList<Questions>();
         this.TagsLists = new LinkedList<Tag>();
     }
-    public static QuestionBank GetInstance() {
+    protected static QuestionBank GetInstance() {
         if (instance == null) {
             instance = new QuestionBank();
         }
@@ -24,7 +24,7 @@ public class QuestionBank {
      * Method that returns the list of all questions
      * @return A list containing all questions
      */
-    public LinkedList<Questions> GetQuestions(){
+    protected LinkedList<Questions> GetQuestions(){
         return this.Questions;
     }
 
@@ -32,7 +32,7 @@ public class QuestionBank {
      * Method that returns a list of all tags
      * @return A list containing all tags
      */
-    public LinkedList GetTagsList(){
+    protected LinkedList GetTagsList(){
         return this.TagsLists;
     }
 
@@ -40,7 +40,7 @@ public class QuestionBank {
      * Method that creates a question, based on user input
      * @return True if the question was created successfully; False otherwise
      */
-    public Boolean CreateQuestion(int typeChoice, String questionDescription, LinkedList<String> correctAnswers, LinkedList<String> possibleAnswers){
+    protected Boolean CreateQuestion(int typeChoice, String questionDescription, LinkedList<String> correctAnswers, LinkedList<String> possibleAnswers){
         int lastUsedId; // Variable to hold last used id number for a question
         if (this.Questions.peekLast() == null) { // Check if any questions have been added
             // If no questions yet, set lastUsedId to -1
@@ -68,117 +68,6 @@ public class QuestionBank {
         // Once done, add new question to this.Questions
         return this.Questions.add(question);
     }
-
-    /**
-     * Method that gets user input on what kind of question to make
-     * @return Int corresponding to what kind of question to make
-     */
-    /*
-    private int GetUserQuestionType() {
-        boolean incorrectInput = true;
-        int typeChoice = -1;
-        while (incorrectInput) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Please select what kind of question:\n" +
-                        "enter 1 for multiple choice with only one answer\n" +
-                        "enter 2 for multiple choice with several answers\n" +
-                        "enter 3 for short answer\n" +
-                        "Your choice: ");
-                String input = scanner.nextLine();
-                scanner.close();
-                try {
-                    typeChoice = Integer.parseInt(input);
-                } catch (NumberFormatException e) {
-                    System.out.println("Please enter only a number.");
-                    typeChoice = -1;
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            if (typeChoice == 1 || typeChoice == 2 || typeChoice == 3) {
-                incorrectInput = false; // break our of while loop
-            }
-        }
-        return typeChoice;
-    }
-
-    /**
-     * Method that gets user input for the question description
-     * @return String of question description
-     */
-    /*
-    private String GetUserQuestionDescription() {
-        boolean incorrectInput = true;
-        String description = null;
-        while (incorrectInput) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Please enter the question description:\n" +
-                        "Description: ");
-                description = scanner.nextLine();
-                scanner.close();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        return description;
-    }
-
-    /**
-     * Method that gets user input for the correct answers to the question
-     * @return A list of correct answers for the question
-     */
-    /*
-    private LinkedList<String> GetUserCorrectAnswers() {
-        LinkedList<String> correctAnswers = new LinkedList<>();
-        while (true) {
-            String input = null;
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Please a correct answer:\n" +
-                        "enter 'end' when you are finished adding answers\n" +
-                        "Answer: ");
-                input = scanner.nextLine();
-                scanner.close();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            if (input == "end" || input == "End") {
-                break; // break out of while loop
-            }
-            correctAnswers.add(input);
-        }
-        return correctAnswers;
-    }
-
-    /**
-     * Method that gets user input for possible answers for the question
-     * @return A list of possible answers
-     */
-    /*
-    private LinkedList<String> GetUserPossibleAnswers() {
-        LinkedList<String> possibleAnswers = new LinkedList<>();
-        while (true) {
-            String input = null;
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.println("Please a possible answer:\n" +
-                        "enter 'end' when you are finished adding answers\n" +
-                        "Answer: ");
-                input = scanner.nextLine();
-                scanner.close();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            if (input == "end" || input == "End") {
-                break; // break out of while loop
-            }
-            possibleAnswers.add(input);
-        }
-        return possibleAnswers;
-    }
-    */
 
     /**
      * Method that makes a question based on specified inputs
@@ -215,7 +104,7 @@ public class QuestionBank {
      * @param ID The ID number of the question being searched for
      * @return True if the question was successfully removed; False if question not found
      */
-    public Boolean RemoveQuestion(int ID){
+    protected Boolean RemoveQuestion(int ID){
         for (Questions q : this.Questions) {
             if (q.GetIdNumber() == ID) {
                 return Questions.remove(q);
@@ -229,7 +118,7 @@ public class QuestionBank {
      * @param ID The ID number of the question being searched for
      * @return The question with the specified ID number; Null if question not found
      */
-    public Questions GetQuestion(int ID){
+    protected Questions GetQuestion(int ID){
         for (Questions q : this.Questions) {
             if (q.GetIdNumber() == ID) {
                 return q;
@@ -243,7 +132,7 @@ public class QuestionBank {
      * @param tagname The name of the tag to search for questions of
      * @return A list of all questions with the specified tag; Null if tag not found
      */
-    public LinkedList<Questions> GetTaggedQuestions(String tagname) {
+    protected LinkedList<Questions> GetTaggedQuestions(String tagname) {
         LinkedList<Questions> taggedQuestions = new LinkedList<Questions>();
 
         // Search through this.TagsList to find the specified tag
@@ -273,7 +162,7 @@ public class QuestionBank {
      * @param tagName The name of the tag to be removed
      * @return True if found and removed correctly; False otherwise
      */
-    public boolean RemoveTag(String tagName) {
+    protected boolean RemoveTag(String tagName) {
         // Search through this.TagsList to find specified tag
         for (Tag t : this.TagsLists) {
             if(t.GetTagName() == tagName) { // Found tag
@@ -295,5 +184,13 @@ public class QuestionBank {
         }
         // Specified tag not found
         return false;
+    }
+
+    /**
+     * Method that resets the question bank to a default, empty state
+     */
+    protected void ResetBank() {
+        this.Questions = new LinkedList<Questions>();
+        this.TagsLists = new LinkedList<Tag>();
     }
 }
