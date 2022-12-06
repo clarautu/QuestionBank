48779a9af4d40f4d44d5f2808385f7dbcae1d534
Autumn Clark <clarautu@isu.edu>
2022-12-06 13:15:25 -0700

    Added javadoc to class Questions

--- app/src/main/java/QuestionBank/Questions.java
+++ app/src/main/java/QuestionBank/Questions.java
@@ -19,37 +19,83 @@ public abstract class Questions {
 
     public Questions() {} // no args constructor for custom gson deserialization builder
 
-
+    /**
+     * Method that gets the ID number for the question
+     * @return The ID number for this question
+     */
     protected int GetIdNumber(){return this.IdNumber;}
+
+    /**
+     * Method that gets the prompt for this question
+     * @return The prompt/description of this question
+     */
     protected String GetQuestion(){return this.Question;}
+
+    /**
+     * Method that gets all the correct answers for this question
+     * @return A list of correct answers for this question
+     */
     protected LinkedList<String> GetCorrectAnswer(){return this.CorrectAnswers;}
+
+    /**
+     * Method that gets all the possible answers for this question
+     * @return A list of possible answers for this question
+     */
     protected LinkedList<String> GetPossibleAnswers(){return this.PossibleAnswers;}
+
+    /**
+     * Method that gets all the tags for this question
+     * @return A list of all the tags for this question
+     */
     protected LinkedList<String> GetTags(){return this.Tags;}
+
+    /**
+     * Method that updates the prompt for this question
+     * @param question The new prompt
+     * @return true if the update was successful; False otherwise
+     */
     protected boolean SetQuestion(String question){
         Question = question;
         return true;
     }
+
+    /**
+     * Method that adds a new possible answer to the question
+     * @param answer The new possible answer to add
+     * @return True if it was added successfully; False otherwise
+     */
     protected boolean AddPossibleAnswer(String answer){
         for(String ans : PossibleAnswers){
             if (ans == answer){return false;} //If already identical answer, then return false without adding
         }
         return PossibleAnswers.add(answer);
     }
-    protected boolean RemoveAnswer(String answer){
-        CorrectAnswers.remove(answer);
-        return PossibleAnswers.remove(answer);
-    }
+
+    /**
+     * Method that adds a new correct answer to the question
+     * @param CorrectAnswer The new correct answer to add
+     * @return True if it was added successfully; False otherwise
+     */
     protected boolean AddCorrectAnswer(String CorrectAnswer){
         AddPossibleAnswer(CorrectAnswer); //I decided to have it automatically add the answer to PossibleAnswers
         //the AddPossibleAnswer method will catch if there is already an identical answer and ignore it
         return CorrectAnswers.add(CorrectAnswer);
     }
-    protected boolean RemoveCorrectAnswer(String answer){
-        return CorrectAnswers.remove(answer);
-    }
+
+    /**
+     * Method that adds a tag to the question
+     * @param tagName The name of the tag to add
+     * @return True if it was added successfully; False otherwise
+     */
     protected boolean AddTag(String tagName){
         return Tags.add(tagName);
     }
+
+    /**
+     * Method that removes a specified tag from the question
+     * @param tagName The name of the tag to be removed
+     * @return True if it was updated successfully; False otherwise
+     */
     protected boolean RemoveTag(String tagName){
         return Tags.remove(tagName);
     }
