c09bf1935c5833585b211129c23ac943db4f89dc
Eric Hill <hilleri2@isu.edu>
2022-12-06 14:26:05 -0700

    Fixed adding tags to questions

--- app/src/main/java/QuestionBank/AddMultipleAnswerSubScreen.java
+++ app/src/main/java/QuestionBank/AddMultipleAnswerSubScreen.java
@@ -259,18 +259,31 @@ public class AddMultipleAnswerSubScreen {
         private void AddTag(){
             //BankFacade.GetInstance().AddTagToQuestion(question.GetIdNumber(),(String)tagsListBox.getSelectedItem());
             tagsAdded.add((String)tagsListBox.getSelectedItem());
-
-            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-            Prepare();
-            Show();
+            //reset add tags jbox
+            tagsListBox.removeAllItems();
+            LinkedList<Tag> tags = BankFacade.GetInstance().GetTagsList();
+            tagsListBox.setEditable(true);
+            for (Tag t : tags) {
+                String name = t.GetTagName();
+                int maxLength = 20;
+                if (name.length() > maxLength) {
+                    name = name.substring(0, 20);
+                }
+                tagsListBox.addItem(name);
+            }
+            //reset remove tags jbox
+            removeTagSelection.removeAllItems();
+            for (String str : tagsAdded) {
+                removeTagSelection.addItem(str);
+            }
         }
         private void RemoveTag(){
             //BankFacade.GetInstance().RemoveTagFromQuestion(question.GetIdNumber(),(String) removeTagSelection.getSelectedItem());
             tagsAdded.remove((String) removeTagSelection.getSelectedItem());
-            //UpdateTagPanel();
-            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-            Prepare();
-            Show();
+            removeTagSelection.removeAllItems();
+            for (String str : tagsAdded) {
+                removeTagSelection.addItem(str);
+            }
         }
     }
 }
